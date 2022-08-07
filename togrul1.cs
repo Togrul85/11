40995a7c88215b7c42b53839562d821bb6bfe4cd
unknown <toghrul@code.edu.az>
2022-08-07 22:02:53 +0400

    .

---
+++ togrul1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 190, 510, 389 };
+            Console.WriteLine(count);
+            
+        }
+
+    }
+    static int NumLength(int num, int temp, int count)
+    {
+        count = 1;
+        temp = num;
+        {
+            if (temp > 9 || temp < -9)
+            {
+                temp = temp / 10;
+                count++;
+
+               
+               
+            }
+            return (count);
+            {
+
+            } 
+        }
+
+
+    }
+     
+
+
+    }
+    
+
+
+
+
+
+
